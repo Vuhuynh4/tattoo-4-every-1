f31ec55f2b850e67d4824b821215ba5aa9a53f80
VuHuynh <vu.huynh@mservice.com.vn>
2019-12-21 10:59:53 +0700

    Bold the currently selected item

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -47,13 +47,21 @@ class Game extends Component {
 
         const moves = history.map((step, move) => {
             const desc = move ?
-                'Move #' + move + ' (Col: ' + step.col + ' - Row: ' + step.row + ')':
-                'Restart';
-            return (
-                <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
-                </li>
-            );
+                    'Move #' + move + ' (Col: ' + step.col + ' - Row: ' + step.row + ') isBold: ' + this.state.stepNumber :
+                    'Restart';
+            if (this.state.stepNumber == move) {
+                return (
+                    <li key={move}>
+                        <button onClick={() => this.jumpTo(move)}><b>{desc}</b></button>
+                    </li>
+                );
+            } else {
+                return (
+                    <li key={move}>
+                        <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    </li>
+                );
+            }
         });
 
         let status;
